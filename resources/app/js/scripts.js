03ec30cc17b93bd849a76137085da9287f68fad4
Josh Peizer <github@sent.sh>
2021-03-05 08:06:13 -0500

    Theme Added

---
+++ resources/app/js/scripts.js
@@ -0,0 +1,27 @@
+(function(window, document) {
+    'use strict';
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     const buttons = document.getElementsByClassName('fas');
+    //     for (const button of buttons) {
+    //         const form = button.nextElementSibling
+    //         button.addEventListener('click', (event) => {
+    //             form.submit()
+    //         });
+    //     }
+    // })
+
+    // let hiddenInput = document.getElementById('serial');
+    //
+    // let focusHiddenArea = function() {
+    //     hiddenInput.val(' ');
+    //     hiddenInput.focus().select();
+    // };
+
+    // ['cut', 'copy', 'paste'].forEach(function(event) {
+    //     document.addEventListener(event, function(e) {
+    //         console.log(event);
+    //         focusHiddenArea();
+    //         e.preventDefault();
+    //     });
+    // });
+})(window, document);
